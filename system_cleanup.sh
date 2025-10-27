0288e5dcb1e02547446d1066319fc0d81773d3cb
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-10-27 10:37:30 +0300

    progress bars

--- system_cleanup.sh
+++ system_cleanup.sh
@@ -3,7 +3,6 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
-YELLOW='\033[1;33m'
 NC='\033[0m'
 
 log_file="/home/$USER/script.log"
@@ -52,6 +51,7 @@ if sudo find /var/log -name "*.log" -mtime +7 | grep -q .; then
 	echo -e "${GREEN}logs older than 7 days:${NC}"
 	sudo find /var/log -name "*.log" -mtime +7 -exec du -h {} \; | sort -hr | awk '{print "    "$0}'
 	sudo find /var/log -name "*.gz" -mtime +7 -exec du -h {} \; | sort -hr | awk '{print "    "$0}'
+
 	#deleting logs older than 7 days
 	while true; do
 		read -p "Delete outdated log files? (older than 7 days) [Y/n] " -r
@@ -61,11 +61,32 @@ if sudo find /var/log -name "*.log" -mtime +7 | grep -q .; then
 			echo -e "${RED}Deleting outdated logs...${NC}"
 			#sudo find /var/log -name "*.log" -mtime +7 -delete
 			#sudo find /var/log -name "*.gz" -mtime +7 -delete
-			sleep 1.5
+
+			log_files=($(sudo find /var/log \( -name "*.log" -o -name "*.gz" \) -mtime +7))
+			all_log_files=${#log_files[@]}
+			removed_logs=0
+
+			for file in "${log_files[@]}"; do
+				#sudo rm -f "$file"
+				removed_logs=$((removed_logs + 1))
+				
+				local percent_logs_removed=$((removed_logs * 100 / all_log_files))
+
+				filled=$((percent_log_removed / 2))
+				empty=$((50 - filled))
+
+				printf "\r${GREEN}[%-50s]${NC} %d%%" "$(printf '█%.0s' $(seq 1 $filled))$(printf '░%.0s' $(seq 1 $empty))" "$i" 
+
+			done
+			sleep 0.3
+			echo ""
+			echo -e "${RED}Done${NC}"
 			break
-		elif [[ $REPLY =~ [N/n]$ ]]; then
+
+		elif [[ $REPLY =~ ^[Nn]$ ]]; then
 			echo -e "${RED}You have selected to keep outdated logs${NC}"
 			break
+
 		else 
 			echo -e "${RED}Invalid input $REPLY. Please try again${NC}"
 		fi
@@ -91,10 +112,23 @@ if sudo journalctl --until "7 days ago" --quiet --no-pager | grep -q .; then
 		
 		if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
 			echo -e "${RED}Deleting journal entries older than 7 days...${NC}"
-			#sudo journalctl --vacuum-time=7d
-			sleep 1.5
+
+			for i in {0..100}; do
+				
+				if [[ $i -eq 10 ]]; then
+					sudo journalctl --vacuum-time=7d 
+				fi
+
+				filled=$((i / 2))
+				empty=$((50 - filled))
+
+				printf "\r${GREEN}[%-50s]${NC} %d%%" "$(printf '█%.0s' $(seq 1 $filled))$(printf '░%.0s' $(seq 1 $empty))" "$i" 
+			done
+			sleep 0.3
+			echo ""
+			echo -e "${RED}Done${NC}"
 			break
-		elif [[ $REPLY =~ [N/n]$ ]]; then
+		elif [[ $REPLY =~ ^[Nn]$ ]]; then
 			echo -e "${RED}You have selected to keep outdated journal entries${NC}"
 			break
 		else 
@@ -127,12 +161,34 @@ if [[ $(sudo du -s /var/tmp | cut -f1) -gt 0 ]] || [[ $(sudo du -s /tmp | cut -f
 		
 		if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
 			echo -e "${RED}Deleting temporary files${NC}"
-			#sudo find /tmp -type f -delete
-			#sudo find /var -type f -delete
-			
-			sleep 1
+
+			tmp_files=($(sudo find /tmp /var/tmp -type f))
+			all_tmp_files=${#tmp_files[@]}
+			removed_tmp_files=0
+
+			for file in "${tmp_files[@]}"; do
+				#sudo rm -f "$file"
+				removed_tmp_files=$((removed_tmp_files + 1))
+				
+				local percent__tmp_removed=$((removed_tmp_files * 100 / all_tmp_files))
+				
+				#if [[$i -eq 20]]; then
+					#sudo find /tmp -type f -delete
+				#elif [[$i -eq 40]]; then
+					#sudo find /var/tmp -type f -delete
+				#fi
+
+				filled=$((percent_tmp_removed / 2))
+				empty=$((50 - filled))
+
+				printf "\r${GREEN}[%-50s]${NC} %d%%" "$(printf '█%.0s' $(seq 1 $filled))$(printf '░%.0s' $(seq 1 $empty))" "$i" 
+
+			done
+			sleep 0.3
+			echo ""
+			echo -e "${RED}Done${NC}"
 			break
-		elif [[ $REPLY =~ [N/n]$ ]]; then
+		elif [[ $REPLY =~ ^[Nn]$ ]]; then
 			echo -e "${RED}You have selected to keep temporary files${NC}"
 			break
 		else 
@@ -150,13 +206,12 @@ echo""
 echo -e "${BLUE}Checking for cache...${NC}"
 sleep 1
 echo ""
-if [[ $(sudo du -h -sh /var/cache/apt/archives/ | cut -f1) -gt 0]] || [[ $(sudo du -h -sh ~/.cache -gt 0 | cut -f1) -gt 0]] || [[$(sudo du -h -sh ~/.local/share/Trash | cut -f1) -gt 0]] || [[$(sudo du -h -sh ~/.Thumbnails| cut -f1) -gt 0]]; then
+if [[ $(sudo du -s /var/cache/apt/archives/ | cut -f1) -gt 0 ]] || [[ $(sudo du -s ~/.cache | cut -f1) -gt 0 ]] || [[ $(sudo du -s ~/.local/share/Trash | cut -f1) -gt 0 ]] || [[ $(sudo du -s ~/.Thumbnails| cut -f1) -gt 0 ]]; then
 
 	echo -e "${GREEN}Cache:${NC}"
-	sudo du -h -sh /var/cache/apt/archives/ | awk '{print "    "$0}'
-	sudo du -h -sh ~/.cache | awk '{print "    "$0}'
-	sudo du -h -sh ~/.local/share/Trash | awk '{print "    "$0}'
-	sudo du -h -sh ~/.Thumbnails| awk '{print "    "$0}'
+	sudo du -sh /var/cache/apt/archives/ | awk '{print "    "$0}'
+	sudo du -sh ~/.cache | awk '{print "    "$0}'
+	sudo du -sh ~/.local/share/Trash | awk '{print "    "$0}'
 	echo ""
 
 	#Deleting cache
@@ -166,17 +221,40 @@ if [[ $(sudo du -h -sh /var/cache/apt/archives/ | cut -f1) -gt 0]] || [[ $(sudo 
 		
 		if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
 			echo -e "${RED}Deleting cache...${NC}"
-			#rm -rf ~/.cache/*
-			#sudo apt clean
-			#sudo apt autoclean
-			#sudo apt autoremove
-			#sudo find /var/cache/apt/archives/ -type f -mtime +14 -delete 2>/dev/null
-			#rm -rf ~/.local/share/Trash/*
-			sleep 1
+
+			cache_files=($(sudo find ~/.cache ~/.local/share/Trash /var/cache/apt/archives/ -type f ))
+			all_cache_files=${#cache_files[@]}
+			removed_cache=0
+
+			for file in "${tmp_files[@]}"; do
+				#sudo rm -f "$file"
+				removed_cache=$((removed_cache + 1))
+				
+				local percent_cache_removed=$((removed_cache * 100 / all_cache_files))
+				
+				#if [[percent_cache_removed -eq 20]]; then
+					#sudo apt clean
+				#elif [[percent_cache_removed  -eq 40]]; then
+					#sudo apt autoclean
+				#elif [[percent_cache_removed  -eq 60]]; then
+					#sudo apt autoremove
+				#fi
+
+				filled=$((percent_cache_removed / 2))
+				empty=$((50 - filled))
+
+				printf "\r${GREEN}[%-50s]${NC} %d%%" "$(printf '█%.0s' $(seq 1 $filled))$(printf '░%.0s' $(seq 1 $empty))" "$i" 
+
+			done
+			sleep 0.3
+			echo ""
+			echo -e "${RED}Done${NC}"
 			break
-		elif [[ $REPLY =~ [N/n]$ ]]; then
+
+		elif [[ $REPLY =~ ^[Nn]$ ]]; then
 			echo -e "${RED}You have selected to keep cache${NC}"
 			break
+			
 		else 
 			echo -e "${RED}Invalid input $REPLY. Please try again${NC}"
 		fi
@@ -189,7 +267,8 @@ echo ""
 
 #performing system update
 
-echo -e "${BLUE}updating package lists..${NC}"
+echo -e "${BLUE}updating package lists...${NC}"
+echo ""
 sudo apt update
 echo ""
 echo -e "${BLUE}Checking for available updates...${NC}"
@@ -214,7 +293,7 @@ while true; do
 				echo -e "${RED}Rebooting...${NC}"
 				#sudo reboot
 				break
-			elif [[ $REPLY =~ [N/n]$ ]]; then
+			elif [[ $REPLY =~ ^[Nn]$ ]]; then
 				echo -e "${RED}exiting...${NC}"
 				sleep 1
 				break
@@ -223,7 +302,7 @@ while true; do
 			fi
 		done
 		break
-	elif [[ $REPLY =~ [N/n]$ ]]; then
+	elif [[ $REPLY =~ ^[Nn]$ ]]; then
 		echo -e "${RED}exiting...${NC}"
 		sleep 1
 		break
